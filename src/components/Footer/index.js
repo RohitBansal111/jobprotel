28c0d34d3575b30543c31be7bb0848ede7542365
appweb123 <appweb.live@gmail.com>
2022-07-28 09:34:12 +0530

    fix UI

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
      return (
           <footer className="footer-main">
                <div className="container">
-                    <p>© 2021 - 2022 Job Portal® Global Inc.</p>
+                    <p>© 2021 - 2022 Job Portal ®Global Inc.</p>
                </div>  
           </footer>
      )
